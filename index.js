4802f72d6cb32720458f6d376aedcd6c5cfb230e
Anand Thakker <vestibule@anandthakker.net>
2016-05-03 10:41:41 -0400

    Smoother throttling

--- index.js
+++ index.js
@@ -34,12 +34,17 @@ function datascope (map, options) {
   }, options)
 
   var container = yo`<div class="mapboxgl-datascope"></div>`
-  map.on(options.event, throttle(showDataAtPoint, 30))
+
+  var showDataAtPoint = throttle(_showDataAtPoint, 100)
+  map.on(options.event, function (e) {
+    if (options.popup) { options.popup.setLngLat(e.lngLat) }
+    showDataAtPoint(e)
+  })
 
   var draw
   var summaryData = {}
   var selectedAreas = []
-  var updateSummaries = throttle(_updateSummaries, 50)
+  var updateSummaries = throttle(_updateSummaries, 100)
   if (options.summaries) {
     draw = Draw({ controls: { line_string: false, point: false } })
     map.addControl(draw)
@@ -73,9 +78,7 @@ function datascope (map, options) {
       updateSelectedAreas(selectedAreas.filter((id) => removed.indexOf(id) < 0))
     })
 
-    map.on('draw.changed', function (e) {
-      updateSummaries()
-    })
+    map.on('draw.changed', function (e) { updateSummaries() })
   }
 
   return container
@@ -100,7 +103,7 @@ function datascope (map, options) {
     })
   }
 
-  function showDataAtPoint (e) {
+  function _showDataAtPoint (e) {
     var features = map.queryRenderedFeatures(e.point, {
       radius: options.radius,
       layers: options.layers.concat([
@@ -127,7 +130,6 @@ function datascope (map, options) {
 
     if (options.popup) {
       options.popup.setDOMContent(container)
-      options.popup.setLngLat(e.lngLat)
       options.popup.addTo(map)
     }
   }
